b49d3b4556a0c79624d495a067f34469276f54ed
Aditya <adityapandeyadp@gmail.com>
2024-12-09 03:16:13 +0530

    auth user for reports

--- backend/routes/reports.ts
+++ backend/routes/reports.ts
@@ -1,20 +1,40 @@
 import { Router } from "express";
-import { authenticate } from "../middleware/authenticator";
-import Form from "../models/Form";
 import ResponseModel from "../models/Responses";
+import { authenticate } from "../middleware/authenticator";
 
 const router = Router();
 
 // GET all forms responses by id
 
-router.get("/form/:id", async(req, res) => {
+router.get("/form/:id",authenticate, async (req, res):Promise<any> => {
 
-    const { id } = req.params;
-    const responses = await ResponseModel.find({ formId: id });
-    res.json({
-        responses: responses,
-        length: responses.length
-    });
+    try {
+        const { id } = req.params;
+
+        const responses = await ResponseModel.find({ formId: id });
+
+        if (responses.length === 0) {
+
+            return res.status(404).json({
+                message: "No responses found"
+            });
+        }
+        else {
+
+            res.json({
+                responses: responses,
+                length: responses.length
+            });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Internal server error",
+            error
+        });
+
+    }
 });
 
 export const reports = router;
