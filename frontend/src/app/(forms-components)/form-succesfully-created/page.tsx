358f4e7625ffe693d9c0a23883b6ceed1f757ebe
Aditya <adityapandeyadp@gmail.com>
2024-12-15 20:44:56 +0530

    success page is doone

--- frontend/src/app/(forms-components)/form-succesfully-created/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const From_Succesfully_Created = () => {
-  return (
-    <div>From_Succesfully_Created</div>
-  )
-}
-
-export default From_Succesfully_Created
---
+++ frontend/src/app/(forms-components)/success/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function layout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="mt-20">
+            {children}
+        </div>
+
+    )
+}
