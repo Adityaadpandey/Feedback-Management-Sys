f5a7a3faae52b91ba5608d0ba54e7ae8c22430e3
Aditya <adityapandeyadp@gmail.com>
2024-12-22 15:04:25 +0530

    code optimization

--- frontend/src/components/forms/question-types/Dropdown.tsx
+++ frontend/src/components/forms/question-types/Dropdown.tsx
@@ -4,18 +4,18 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { QuestionProps } from "@/types/form";
 
 export function Dropdown({ question, onChange, value }: QuestionProps) {
-  return (
-    <Select value={value} onValueChange={(val) => onChange(question._id, val)}>
-      <SelectTrigger className="w-full">
-        <SelectValue placeholder="Select an option" />
-      </SelectTrigger>
-      <SelectContent>
-        {question.options?.map((option) => (
-          <SelectItem key={option} value={option}>
-            {option}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  );
+    return (
+        <Select value={value} onValueChange={(val) => onChange(question._id, val)}>
+            <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select an option" />
+            </SelectTrigger>
+            <SelectContent>
+                {question.options?.map((option) => (
+                    <SelectItem key={option} value={option}>
+                        {option}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    );
 }
--- frontend/src/components/forms/question-types/ShortAnswer.tsx
+++ frontend/src/components/forms/question-types/ShortAnswer.tsx
@@ -4,12 +4,12 @@ import { Input } from "@/components/ui/input";
 import { QuestionProps } from "@/types/form";
 
 export function ShortAnswer({ question, onChange }: QuestionProps) {
-  return (
-    <Input
-      type="text"
-      placeholder="Type your answer"
-      className="w-full border rounded-md p-2"
-      onChange={(e) => onChange(question._id, e.target.value)}
-    />
-  );
+    return (
+        <Input
+            type="text"
+            placeholder="Type your answer"
+            className="w-full border rounded-md p-2"
+            onChange={(e) => onChange(question._id, e.target.value)}
+        />
+    );
 }
