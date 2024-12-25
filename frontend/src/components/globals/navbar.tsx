c19958fe25bc05432e5930b8ac7656be2bef4792
Aditya <adityapandeyadp@gmail.com>
2024-12-25 13:06:22 +0530

    indensts

--- frontend/src/components/globals/navbar.tsx
+++ frontend/src/components/globals/navbar.tsx
@@ -1,28 +1,28 @@
-import {  LayoutDashboard, Home, PlusIcon } from "lucide-react";
+import { Home, LayoutDashboard, PlusIcon } from "lucide-react";
 import { FloatingNav } from "../ui/floating-navbar";
 
 const Navbar = () => {
-  return (
-    <FloatingNav
-      navItems={[
-        {
-          name: "Home",
-          link: "/",
-          icon: <Home size={24} />,
-        },
-        {
-          name: "DashBoard",
-          link: "/dashboard",
-          icon: <LayoutDashboard size={24} />,
-              },
-        {
-          name: "Create Form",
-          link: "/form-builder",
-          icon: <PlusIcon size={24} />,
-              },
-      ]}
-    />
-  );
+    return (
+        <FloatingNav
+            navItems={[
+                {
+                    name: "Home",
+                    link: "/",
+                    icon: <Home size={24} />,
+                },
+                {
+                    name: "DashBoard",
+                    link: "/dashboard",
+                    icon: <LayoutDashboard size={24} />,
+                },
+                {
+                    name: "Create Form",
+                    link: "/form-builder",
+                    icon: <PlusIcon size={24} />,
+                },
+            ]}
+        />
+    );
 };
 
 export default Navbar;
